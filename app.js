dbe000bf01b57c684c53e60ed5aedcc57ea808c6
Eider Pool <eider.pool.arjona@gmail.com>
2023-03-23 22:10:19 -0600

    evento connection de socket.io escuchando

--- app.js
+++ app.js
@@ -4,9 +4,20 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
+const socketio = require('socket.io');
+const io = socketio(server);
 
 
 app.use(express.static('public'));
+
+
+io.on('connection', (socket) => {
+  console.log(`El cliente ${socket.id} se a conectado`);
+
+  socket.on('disconnect', () => {
+    console.log('Un cliente se ha desconectado');
+  });
+});
 
 
 server.listen(3000, () => {
