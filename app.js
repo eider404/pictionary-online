e9a534bf4f9c18d3026aa570eb30e63623ffa659
Eider Pool <eider.pool.arjona@gmail.com>
2023-03-23 22:30:45 -0600

    evento coordinateSlider agregado

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ app.use(express.static('public'));
 io.on('connection', (socket) => {
   console.log(`El cliente ${socket.id} se ha conectado :)`);
 
+  //recibe las coordenadas del plano a pintar y un color
+  socket.on('coordinateSlider', (x, y, color) => {
+    /* console.log(`${x} / ${y} / ${color}`); */
+    io.emit('dibujar', x , y , color);  
+  });
+
+
+
   socket.on('disconnect', () => {
     console.log(`El cliente ${socket.id} se ha desconectado :(`);
   });
