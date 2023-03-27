7ac71c5c18a86adad57e010fef8de2500af6eb74
Eider Pool <eider.pool.arjona@gmail.com>
2023-03-27 00:32:51 -0600

    formulario 'verificar animal' conectado con el backend

--- public/fetchs.js
+++ public/fetchs.js
@@ -11,3 +11,34 @@ function GenerarAnimal(){
         })
         .catch(err => console.log(err));
 }
+
+
+function Comprobar(){
+    let form = document.querySelector( '#form-comprobar' )
+
+    const obj = {}
+    new FormData( form ).forEach( ( value, key ) => obj[ key ] = value )
+    
+    fetch( `http://localhost:3000/verificar-animal`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify( obj )
+        })
+        .then(res => res.json())
+        .then( data =>{
+            if(data.messaje == 'Correcto'){
+                document.querySelector("#animalGenerado").innerHTML= `
+                    ${data.data} es ${data.messaje}
+                `;
+                document.querySelector("#mensaje").style.color="#4BB543";
+            }else{
+                document.querySelector("#animalGenerado").innerHTML= `
+                    ${data.data} es ${data.messaje}
+                `;
+                document.querySelector("#mensaje").style.color="#891a1a";
+            }
+        })
+        .catch(err => console.log(err));
+}
